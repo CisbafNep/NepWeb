0167305dd27b155a8e3a5d3834b74313f68704b2
CisbafNep <nep.plataforma@gmail.com>
2026-02-27 12:53:41 -0300

    update graph UF

--- src/assets/js/about.js
+++ src/assets/js/about.js
@@ -3,85 +3,92 @@
 //gráfico de barras//
 
 const ctx = document.getElementById('alunosChart').getContext('2d');
- let labels = ['Téc de Enfermagem l (2020)', 'Cuidador em Saúde Mental (2020)', 'ACS | Agentes Comunitários de Saúde(2020)', 'TARM (2021)', 'Base SAMU 192 (2021) - Módulo I', 'Base SAMU 192 (2021) - Módulo II', 'Base SAMU 192 (2021) - Módulo III', 'Téc de Enfermagem ll (2021)', 'Téc de Enfermagem lll (2022)', 'Urgências e Emergências Médicas I (2022)', 'HGNI | Acolhimento e Classificação de Risco (2022)', 'Consórcios Públicos (2022)', 'Capacitação para Enfermagem l (2023)', 'Urgências e Emergências Médicas II (2023)', 'TARM (2023)', 'Atualização da Prática Clinica da Enfermagem (2023)', 'Cuidador em Saúde Mental (2024)', 'TARM (2024)', 'Curso APH(2024)', 'Eletrocardiografia Básico (2024)', 'Curso Infecto(2024)', 'Curso Primeiros Socorros - Lei Lucas(2025)', 'Curso APH(2025)', 'AGORA TEM ESPECIALISTAS -  ONCOLOGIA', 'AGORA TEM ESPECIALISTAS -  CARDIOLOGIA', 'AGORA TEM ESPECIALISTAS -  OFTALMOLOGIA', 'AGORA TEM ESPECIALISTAS -  ORTOPEDIA', 'Curso de Sinais Vitais e Escala de Glasgow (2025)', 'Curso de Fundamentos da Enfermagem (2025)','AGORA TEM ESPECIALISTAS -  OTORRINO', 'Curso Primeiros Socorros - Lei Lucas(2026)', 'AGORA TEM ESPECIALISTAS -  ONCOLOGIA 2026', 'Curso Livre  - Rede Alyne 2026' ];
+let labels = ['Téc de Enfermagem l (2020)', 'Cuidador em Saúde Mental (2020)', 'ACS | Agentes Comunitários de Saúde(2020)', 'TARM (2021)', 'Base SAMU 192 (2021) - Módulo I', 'Base SAMU 192 (2021) - Módulo II', 'Base SAMU 192 (2021) - Módulo III', 'Téc de Enfermagem ll (2021)', 'Téc de Enfermagem lll (2022)', 'Urgências e Emergências Médicas I (2022)', 'HGNI | Acolhimento e Classificação de Risco (2022)', 'Consórcios Públicos (2022)', 'Capacitação para Enfermagem l (2023)', 'Urgências e Emergências Médicas II (2023)', 'TARM (2023)', 'Atualização da Prática Clinica da Enfermagem (2023)', 'Cuidador em Saúde Mental (2024)', 'TARM (2024)', 'Curso APH(2024)', 'Eletrocardiografia Básico (2024)', 'Curso Infecto(2024)', 'Curso Primeiros Socorros - Lei Lucas(2025)', 'Curso APH(2025)', 'AGORA TEM ESPECIALISTAS -  ONCOLOGIA', 'AGORA TEM ESPECIALISTAS -  CARDIOLOGIA', 'AGORA TEM ESPECIALISTAS -  OFTALMOLOGIA', 'AGORA TEM ESPECIALISTAS -  ORTOPEDIA', 'Curso de Sinais Vitais e Escala de Glasgow (2025)', 'Curso de Fundamentos da Enfermagem (2025)', 'AGORA TEM ESPECIALISTAS -  OTORRINO', 'Curso Primeiros Socorros - Lei Lucas(2026)', 'AGORA TEM ESPECIALISTAS -  ONCOLOGIA 2026', 'Curso Livre  - Rede Alyne 2026'];
 let inscritos = [112, 182, 734, 54, 528, 528, 528, 613, 409, 305, 96, 219, 391, 232, 54, 634, 383, 19, 1231, 313, 563, 1340, 1371, 314, 338, 86, 100, 844, 698, 120, 550, 91, 563];
- let certificados = [54, 54, 469, 50, 294, 377, 417, 234, 287, 221, 32, 125, 237, 133, 50, 306, 283, 18, 815, 92, 348, 878, 912, 121, 119, 51, 43, 521, 381, 62, 197, 26, 83];
-        
+let certificados = [54, 54, 469, 50, 294, 377, 417, 234, 287, 221, 32, 125, 237, 133, 50, 306, 283, 18, 815, 92, 348, 878, 912, 121, 119, 51, 43, 521, 381, 62, 197, 26, 83];
+
 const alunosChart = new Chart(ctx, {
- type: 'bar',
-data: {
-    labels: labels,
-    datasets: [{
- label: 'Inscritos',
- data: inscritos,
- backgroundColor: 'rgba(84,79,197, 0.6)',
- borderColor: 'rgba(84,79,197, 1)',
-  borderWidth: 1
- },
-{
-label: 'Certificados',
-data: certificados,
-backgroundColor: 'rgba(54, 162, 235, 0.6)',
-borderColor: 'rgba(54, 162, 235, 1)',
-borderWidth: 1
- }
-]
-},
-options: {
-responsive: true,
-plugins: {
-legend: {
-position: 'bottom'
-},
- datalabels: {
-anchor: 'end',
-align: 'top',
-formatter: (value) => value, // Mostra o valor exato
-font: {
- weight: 'bold'
-}
-}
-},
-scales: {
-y: {
- beginAtZero: true
- }
-}
-},
-plugins: [ChartDataLabels] // Ativa o plugin
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Inscritos',
+            data: inscritos,
+            backgroundColor: 'rgba(84,79,197, 0.6)',
+            borderColor: 'rgba(84,79,197, 1)',
+            borderWidth: 1
+        },
+        {
+            label: 'Certificados',
+            data: certificados,
+            backgroundColor: 'rgba(54, 162, 235, 0.6)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }
+        ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            },
+            datalabels: {
+                anchor: 'end',
+                align: 'top',
+                formatter: (value) => value, // Mostra o valor exato
+                font: {
+                    weight: 'bold'
+                }
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    },
+    plugins: [ChartDataLabels] // Ativa o plugin
 });
 
 
 //contador//
 
- function createCounter(elementId, startValue, increment, endValue, interval) {
-     let counter = startValue;
-     function updateCounter() {
-         counter += increment;
-         document.getElementById(elementId).innerText = '+ ' + counter;
-         // Para o contador quando atingir o valor final
-         if (counter >= endValue) {
-             clearInterval(counterInterval);
-         }
-     }
-     // Atualiza o contador no intervalo especificado
-     let counterInterval = setInterval(updateCounter, interval);
- }
- // Criar contadores com diferentes configurações
- createCounter('counter1', 1, 1, 30, 172);
- createCounter('counter2', 1000, 50, 8300, 50);
- createCounter('counter3', 10, 1, 180, 30);
- createCounter('counter4', 1000, 100, 11300, 70);
+function createCounter(elementId, startValue, increment, endValue, interval) {
+    let counter = startValue;
+    function updateCounter() {
+        counter += increment;
+        document.getElementById(elementId).innerText = '+ ' + counter;
+        // Para o contador quando atingir o valor final
+        if (counter >= endValue) {
+            clearInterval(counterInterval);
+        }
+    }
+    // Atualiza o contador no intervalo especificado
+    let counterInterval = setInterval(updateCounter, interval);
+}
+// Criar contadores com diferentes configurações
+createCounter('counter1', 1, 1, 30, 172);
+createCounter('counter2', 1000, 50, 8300, 50);
+createCounter('counter3', 10, 1, 180, 30);
+createCounter('counter4', 1000, 100, 11300, 70);
 
 
 
 //gráfico de mapa//
+// var data = [
+//     ['br-sp', 52], ['br-mg', 44], ['br-rj', 6500], ['br-ba', 13], ['br-go', 4],
+//     ['br-ce', 27], ['br-df', 4], ['br-es', 11], ['br-ms', 10], ['br-al', 5],['br-pi', 4],['br-am', 10],
+//     ['br-pa', 17], ['br-pb', 11],['br-pe', 10], ['br-pr', 16], ['br-rn', 6], ['br-ac', 2],
+//     ['br-ro', 16], ['br-rs', 17], ['br-se', 3], ['br-to', 27], ['br-ap', 2],['br-sc', 9],,['br-ma', 2],
+//     ['br-vazio', 250] // Para estados com valores vazios, se necessário
+// ];
 var data = [
-    ['br-sp', 52], ['br-mg', 44], ['br-rj', 6500], ['br-ba', 13], ['br-go', 4],
-    ['br-ce', 27], ['br-df', 4], ['br-es', 11], ['br-ms', 10], ['br-al', 5],['br-pi', 4],['br-am', 10],
-    ['br-pa', 17], ['br-pb', 11],['br-pe', 10], ['br-pr', 16], ['br-rn', 6], ['br-ac', 2],
-    ['br-ro', 16], ['br-rs', 17], ['br-se', 3], ['br-to', 27], ['br-ap', 2],['br-sc', 9],,['br-ma', 2],
-    ['br-vazio', 250] // Para estados com valores vazios, se necessário
+    ['br-rj', 6855], ['br-mg', 67], ['br-sp', 108], ['br-pr', 30], ['br-rs', 39], ['br-ro', 22], ['br-pa', 67],
+    ['br-to', 31], ['br-ba', 68], ['br-es', 16], ['br-rn', 9], ['br-df', 12], ['br-ms', 12], ['br-pb', 19],
+    ['br-pe', 27], ['br-ce', 188],['br-al', 7],['br-se', 8], ['br-ap', 2], ['br-go', 21],['br-pi', 11],
+    ['br-sc', 18], ['br-am', 15], ['br-ma', 16], ['br-ac', 8], ['br-mt', 5],
+    ['br-vazio', 2500] // Caso queira manter para valores não identificados
 ];
 
 Highcharts.mapChart('mapa', {
@@ -157,19 +164,19 @@ function closeMenu() {
     overlay.classList.remove("show");
 }
 
- // Seleciona o botão
-    const backToTopButton = document.getElementById("backToTop");
+// Seleciona o botão
+const backToTopButton = document.getElementById("backToTop");
 
-    // Mostra/esconde o botão ao rolar a página
-    window.onscroll = function () {
-        if (document.documentElement.scrollTop > 400) {
-            backToTopButton.style.display = "block";
-        } else {
-            backToTopButton.style.display = "none";
-        }
-    };
+// Mostra/esconde o botão ao rolar a página
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 400) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
 
-    // Anima a rolagem suave para o topo
-    backToTopButton.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
+// Anima a rolagem suave para o topo
+backToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
